bfd0774b0945283bb92f565b34b43b05b2460dae
vaibhav <sisodevaibhav11@gmail.com>
2026-01-11 17:10:10 +0530

    feat: organize route imports and declarations in app.js for better readability

--- src/app.js
+++ src/app.js
@@ -17,10 +17,25 @@ app.use(cookieParser());                                  // Middleware for pars
 
 //routes import and usage would go here
 import userRouter from "./routes/user.routes.js";              
- 
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
-app.use("/api/v1/users", userRouter);    // Using userRouter for /api/v1/users routes
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 export default app;
